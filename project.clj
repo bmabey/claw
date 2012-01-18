71be910ceb840d8dbbfa90dc5f1e8999966709ac
Ben Mabey <ben@benmabey.com>
2012-01-17 21:40:02 -0700

    bumps midje to latest.. again, trying to appease CI

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [incanter/parallelcolt "0.9.4"]]
-  :dev-dependencies [[midje "1.3.0-RC2"]
+  :dev-dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]
                      [lein-midje "1.0.7"]])
