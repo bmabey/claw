64acd960708e5955508731eaf49def5c1e9f44f1
Ben Mabey <ben@benmabey.com>
2012-01-17 21:37:29 -0700

    bumps lein-midje to see if that fixes CI build

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
                  [org.clojure/core.incubator "0.1.0"]
                  [incanter/parallelcolt "0.9.4"]]
   :dev-dependencies [[midje "1.3.0-RC2"]
-                     [lein-midje "1.0.4"]])
+                     [lein-midje "1.0.7"]])
