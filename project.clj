95b8876024025c0c49664d878eab3c395882df6d
Ben Mabey <ben@benmabey.com>
2012-01-24 16:48:23 -0700

    turns on reflection by default, don't want any sneaking in...

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                  [org.clojure/core.incubator "0.1.0"]
                  [incanter/parallelcolt "0.9.4"]]
   :dev-dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]
-                     [lein-midje "1.0.7"]])
+                     [lein-midje "1.0.7"]]
+  :warn-on-reflection true)
