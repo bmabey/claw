0c9de3835f5709a9f1f42fb20bfb3afb8f938874
Ben Mabey <ben@benmabey.com>
2012-01-24 17:06:57 -0700

    adds primitive-type macro helpers for dev use

---
+++ test/claw/test/test_helpers.clj
@@ -0,0 +1,23 @@
+(ns claw.test.test-helpers)
+
+(definterface IPrimitiveTester
+  (getType [^int x])
+  (getType [^long x])
+  (getType [^float x])
+  (getType [^double x])
+  (getType [^Object x]))
+
+(deftype PrimitiveTester []
+  IPrimitiveTester
+  (getType [this ^int x] :int)
+  (getType [this ^long x] :long)
+  (getType [this ^float x] :float)
+  (getType [this ^double x] :double)
+  (getType [this ^Object x] :object))
+
+
+(defmacro primitive-type [x]
+  `(.getType (PrimitiveTester.) ~x))
+
+(defmacro pt [x]
+  `(.getType (PrimitiveTester.) ~x))
